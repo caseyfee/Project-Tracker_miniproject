569bba2bf9380637c764c1c7acbf9a5049fd2572
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-01-26 21:25:32 -0800

    Added current time

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 // Parse the following date, 11/3/2020, and convert it into the following format: Sunday, February 14 2010, 3:25:50 pm.
-var reformatDate = dayjs('2020-11-03').format('dddd, MMMM D YYYY, h:mm:ss a');
+var reformatDate = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
 $('#currentDate').text(reformatDate);
